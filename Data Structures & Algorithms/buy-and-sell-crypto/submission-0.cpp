8e893a4c1e133e792f7b317a11f6d47f15b63229
Dylan Sheehan <80593260+Noodl3r@users.noreply.github.com>
2026-06-21 22:35:54 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_sale = prices[0];
+        int best  = 0;
+        for(int i = 0 ;  i < prices.size(); i++){
+            best = max(best, prices[i] - min_sale);
+            min_sale = min(min_sale, prices[i]);
+        }
+        return best;
+    }; 
+};
